2f89c710f4196f4ddc8591b165bfaf509a46e0fe
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-27 15:53:27 -0700

    firs commit for GA Project2 Meatup

---
+++ meatup/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email  = models.CharField(max_length=100)
+    password  = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    interest = models.TextField()
+
+    def __str__(self):
+        return self.username
+
+class Event(models.Model):
+    attendees = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    event_name = models.CharField(max_length=100)
+    event_datetime = models.CharField(max_length=100)
+    event_location = models.CharField(max_length=100)
+    event_description = models.TextField()
+
+
+    def __str__(self):
+        return self.event_name
