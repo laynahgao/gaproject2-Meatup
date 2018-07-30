7fe0791d41f2c40c8a9214f3b16abcf01371a90e
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-30 14:44:27 -0700

    add my first changes to views.py for homepage

--- meatup/views.py
+++ meatup/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import User
+from .models import Event
+
+
+
+def index(request):
+    events = Event.objects.all()
+    return render(request, 'homepage.html', { 'events': events })
