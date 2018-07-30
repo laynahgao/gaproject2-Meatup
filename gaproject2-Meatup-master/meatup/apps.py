a0a7ca0f49c189796c2cda4b3c6a0ce3991b7bcd
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-30 11:25:40 -0700

    fixing the bad commits

--- gaproject2-Meatup-master/meatup/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeatupConfig(AppConfig):
-    name = 'meatup'
--- gaproject2-Meatup-master/meatup/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from.models import user
-
-
-class UserForm(forms.ModelForm):
-  class Meta:
-    model = User
-    fields = ['first_name', 'username', 'email','password', 'interest','photo_url']
