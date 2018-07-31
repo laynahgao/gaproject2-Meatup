98c093fe93419b08991ad1bec7143ebecb01ad05
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-31 14:48:45 -0700

    homepage changes

--- meatup/urls.py
+++ meatup/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('login/', views.login_view, name="login"),
   path('event_homepage/', views.index, name='index'),
+  path('logout/', views.logout_view, name="logout"),
   ]
 
--- meatup_django/urls.py
+++ meatup_django/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meatup.urls'))
+    path('', include('meatup.urls')),
+    path('accounts/login/', auth_views.login, name="login" ),
+    path('accounts/logout/', auth_views.logout, name="logout" ),
 ]
