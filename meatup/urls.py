3e034cde41619cd29525f54bc42020506c98ddaa
Marcus Hunter <Marcusj94@sbcglobal.net>
2018-07-31 14:41:48 -0700

    Merge user and events together
    merge code together

--- meatup/urls.py
+++ meatup/urls.py
@@ -1,1 +1,12 @@
-# add urls
+# main_app/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('event_homepage/', views.index, name='index'),
+  path('user/new', views.user_create, name='user_create'),
+  path('user/<int:id>/', views.user_info, name='user_info'),
+  path('user/<int:id>/edit', views.user_edit, name='user_edit'),
+]
+
--- meatup/views.py
+++ meatup/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, Event
+from .forms import UserForm
 
-# Create your views here.
+
+
+## Homepage##
+
+def index(request):
+    events = Event.objects.all()
+    return render(request, 'homepage.html', { 'events': events })
+
+
+#### User ####
+
+## user profile page ##
+# @login_required
+def user_info(request,id):
+    username = User.objects.get(id=id)
+    print(username)
+    #events = Event.objects.filter
+    return render(request, 'user.html', {'username': username})
+
+# @login_required
+## create ##
+def  user_create(request):
+  if request.method == 'POST':
+    form = UserForm(request.POST)
+    if form.is_valid():
+        user= form.save()
+        return redirect('user_info', id=user.id)
+  else:
+    form = UserForm()
+  return render(request, 'user_form.html', {'form': form})
+
+## edit ##
+
+def  user_edit(request,id):
+  user= User.objects.get(id=id)
+  if request.method == 'POST':
+    form = UserForm(request.POST, instance=user)
+    if form.is_valid():
+        user = form.save()
+        return redirect('user_info', id=user.id)
+  else:
+    form = UserForm(instance=user)
+  return render(request, 'user_form.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
