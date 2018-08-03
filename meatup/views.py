a8f1658a4d17c3fceb8388078a58c899a8d1b770
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-02 19:29:08 -0700

    fix merge conflict in login_view code

--- meatup/views.py
+++ meatup/views.py
@@ -114,27 +114,7 @@ def signup(request):
     return render(request, 'signup.html')
 
 def login_view(request):
-
-  if request.method == 'POST':
-    # if post, then authenticate (user submitted username and password)
-    form = LoginForm(request.POST)
-    if form.is_valid():
-        u = form.cleaned_data['username']
-        p = form.cleaned_data['password']
-        user = authenticate(username = u, password = p)
-        if user is not None:
-            if user. is_active:
-                login(request, user)
-                return redirect('user_info', id=user.id)
-            else:
-                print("The account has been disabled.")
-        else:
-            print("The username and/or password is incorrect.")
-  else:
-      form = LoginForm()
-      return render(request, 'login.html', {'form': form})
-
-  if request.method == 'POST':
+    if request.method == 'POST':
         # if post, then authenticate (user submitted username and password)
         form = LoginForm(request.POST)
 
@@ -153,15 +133,13 @@ def login_view(request):
             else:
                 return render(request, 'login.html', {error: 'Username/password not found'})
 
-  else:
+    else:
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
-
 
 def logout_view(request):
     logout(request)
     return redirect('landing')
-
 
 #### User ####
 
